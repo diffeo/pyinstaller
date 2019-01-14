45dbc2c00bacb31bdb5d3523c48c694b244ad201
kujenga <ataylor0123@gmail.com>
2019-01-14 16:34:12 -0500

    update diffeo-specific version, allow internal PyPi repo

--- setup.py
+++ setup.py
@@ -153,7 +153,7 @@ setup(
     ### DIFFEO MODIFICATION
     # version=version
     # version=version+'+diffeo',
-    version='3.1.1+diffeo',
+    version='3.4+diffeo.1',
     ### END DIFFEO MODIFICATION
 
     description='PyInstaller bundles a Python application and all its '
